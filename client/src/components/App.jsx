0e8e05903d5745cdfeab54e221b4ed26136de246
Jinho Kum <Jinho.kum@gmail.com>
2022-03-19 14:59:46 -0700

    Added Server, webpack, And basic Title.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="title">NBA TEAMS</div>
+      </div>
+    )
+  }
+}
+
+export default App;
